7711c5d0f064531622b0f74ca5453a935e77711a
NwayNwayWai <71000512+NwayNwayWai@users.noreply.github.com>
2025-04-24 02:43:49 +0700

    update final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,21 @@ const inter = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Nway Nway Wai",
-  description: " Frontend Developer",
+  description: "Frontend Developer",
+  icons: {
+    icon: "/favico.ico",
+  },
+  openGraph: {
+    type: "article",
+    url: "https://nwaynwaywai.vercel.app/image/nway1.JPG",
+    title: "Nway Nway Wai",
+    description: "Nway Nway Wai's portfolio",
+    images: [
+      {
+        url: "https://nwaynwaywai.vercel.app/image/nway1.JPG",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -21,29 +35,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-        <title>Nway Nway Wai</title>
-
-        <meta name="description" content="Nway Nway Wai's portfolio" />
-        {/* OPEN GRAPH */}
-
-        <meta property="og:type" content="article" />
-
-        <meta
-          property="og:url"
-          content="https://nwaynwaywai.vercel.app/image/nway1.JPG"
-        />
-
-        <meta property="og:title" content="Nway Nway Wai" />
-
-        <meta property="og:description" content="Nway Nway Wai's portfolio" />
-
-        <meta
-          property="og:image"
-          content="https://nwaynwaywai.vercel.app/image/nway1.JPG"
-        />
-      </head>
       <body className={cn(inter.className, "min-h-screen relative")}>
         <Theme>
           <PageLayout>{children}</PageLayout>
